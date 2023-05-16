67211f33f612fba8e23d50efb82b42967788b60d
jerry-lllman <jerry_lllman@163.com>
2023-05-16 18:42:46 +0800

    chore: add global response interceptors

--- packages/luna-server/src/main.ts
+++ packages/luna-server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 添加全局响应拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
